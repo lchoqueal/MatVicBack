f35145233632cc8f071ca02136929359c6c68596
Luis Anthony de Jesus Choque Alfaro <138828951+lchoqueal@users.noreply.github.com>
2025-10-09 21:58:58 -0500

    Configure CORS with specific origin and credentials

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const productController = require('./controllers/productController');
 const { startListener } = require('./services/dbListener');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('API funcionando'));
