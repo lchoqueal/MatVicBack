6d6444d25400f8b25c77526f5ecf9062210b5601
fabrizzioDLSL <fabrizziodelasota99@gmail.com>
2025-12-09 01:07:27 -0500

    Delete routes directory

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.get('/', userController.list);
-router.get('/:id', userController.getById);
-
-module.exports = router;
